95e89edc4e4581041bad001173d15d20fcea6a9f
eric <contreras.eric18@gmail.com>
2021-08-04 18:41:50 -0400

    commit tree c39551fc9e138255e04e1a50437c37620a0d12a0

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+function findMatching(array, string) {
+    const match = array.filter(n =>
+        n.toUpperCase() === string.toUpperCase());
+
+    if( match !== undefined){
+        return match;
+    }
+
+    return [];
+};
+
+function fuzzyMatch (drivers, string) {
+    const match = drivers.filter(driver =>
+        driver.substring(0,2) === string);
+
+    if( match !== undefined){
+        return match;
+    }
+
+    return [];
+};
+
+function matchName(drivers, string) {
+    const match = drivers.filter(driver =>
+        driver.name === string);
+    
+    if( match !== undefined){
+        return match;
+    }
+
+    return [];
+}
